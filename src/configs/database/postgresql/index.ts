3e29ee25d490c669e444f26c4ec7ef598f4f4b14
Otis Pham <otis@vinova.com.sg>
2024-03-22 16:36:59 +0700

    fix: upgrade connect db configs

--- src/configs/database/postgresql/index.ts
+++ src/configs/database/postgresql/index.ts
@@ -1,8 +1,15 @@
 import { Sequelize, Dialect } from 'sequelize';
 import { config } from 'dotenv';
+import { InternalServerHTTP } from '@src/configs/httpException';
+import logger from '@src/configs/logger';
 import { configDB } from './config.sequelize';
 
 config();
+
+const DB_CONNECT_TIMEOUT = 10 * 1000;
+const DB_RECONNECT_TIMEOUT = 1 * 1000;
+let connectionTimeout: NodeJS.Timeout | null = null;
+let reconnectTimeout: NodeJS.Timeout | null = null;
 
 const sequelize = new Sequelize(configDB.database as string, configDB.username as string, configDB.password, {
   ...configDB,
@@ -12,9 +19,20 @@ const sequelize = new Sequelize(configDB.database as string, configDB.username a
 export const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('> Connection to the database has been established successfully.');
-  } catch (error) {
-    console.error('> Unable to connect to the database:', error);
+    await sequelize.sync({ alter: process.env.NODE_ENV !== 'development' });
+    if (reconnectTimeout) clearTimeout(reconnectTimeout);
+    if (connectionTimeout) clearTimeout(connectionTimeout);
+    logger.log('DB connected');
+  } catch (error: any) {
+    logger.error('DB connection failed, reconnecting...');
+    // try to reconnect
+    reconnectTimeout = setTimeout(connectDB, DB_RECONNECT_TIMEOUT);
+    if (!connectionTimeout) {
+      connectionTimeout = setTimeout(() => {
+        logger.error('DB connection failed', { metadata: error });
+        throw new InternalServerHTTP('DB connection failed');
+      }, DB_CONNECT_TIMEOUT);
+    }
   }
 };
 
