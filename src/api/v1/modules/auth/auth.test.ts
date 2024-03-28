5c67723efd157609cad52129527cdc97b5e1b6ac
Otis Pham <otis@vinova.com.sg>
2024-03-28 13:59:31 +0700

    feat: add unit test for auth module

---
+++ src/api/v1/modules/auth/auth.test.ts
@@ -0,0 +1,178 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import { db } from '@src/configs/database';
+import app from '../../../../configs/express/index';
+
+const req = request(app);
+/** !IMPORTANT: DATABASE SYNC
+ *  Connect and sync database before testing */
+beforeAll(async () => {
+  await db.sequelize.authenticate();
+  await db.sequelize.sync({ alter: true });
+});
+
+afterAll(async () => {
+  await db.sequelize.close();
+});
+
+describe('AUTH MODULE', () => {
+  describe('Register API', () => {
+    const user = {
+      username: 'test',
+      email: 'test@local.com',
+      password: 'test1234',
+      rePassword: 'test1234',
+      first_name: 'test',
+      last_name: 'test',
+    };
+
+    test('should return JWT tokens upon successful registration', async () => {
+      const res = await req.post('/api/v1/auth/register').send(user);
+
+      expect(res.status).toEqual(201);
+      expect(res.body.data.tokens).toHaveProperty('accessToken');
+      expect(res.body.data.tokens).toHaveProperty('refreshToken');
+      expect(res.body.data.user).toHaveProperty('id');
+    });
+
+    test('should return error with status code 400 for existed user', async () => {
+      const res = await req.post('/api/v1/auth/register').send(user);
+
+      expect(res.status).toEqual(400);
+      expect(res.body.message).toEqual('Email or username has already been used.');
+    });
+
+    test('should return error with status code 400 for invalid payload', async () => {
+      user.email = 'test1@.com';
+      const res = await req.post('/api/v1/auth/register').send(user);
+
+      expect(res.status).toEqual(400);
+      expect(res.body.message).toEqual('"email" must be a valid email');
+    });
+  });
+
+  describe('Login API', () => {
+    test('should return JWT tokens upon successful login by email', async () => {
+      const dto = {
+        email: 'test@local.com',
+        password: 'test1234',
+      };
+      const res = await req.post('/api/v1/auth/login').send(dto);
+
+      expect(res.status).toEqual(200);
+      expect(res.body.data.tokens).toHaveProperty('accessToken');
+      expect(res.body.data.tokens).toHaveProperty('refreshToken');
+      expect(res.body.data.user).toHaveProperty('id');
+    });
+
+    test('should return JWT tokens upon successful login by username', async () => {
+      const dto = {
+        username: 'test',
+        password: 'test1234',
+      };
+      const res = await req.post('/api/v1/auth/login').send(dto);
+
+      expect(res.status).toEqual(200);
+      expect(res.body.data.tokens).toHaveProperty('accessToken');
+      expect(res.body.data.tokens).toHaveProperty('refreshToken');
+      expect(res.body.data.user).toHaveProperty('id');
+    });
+
+    test('should return error with status code 400 for wrong password or email', async () => {
+      const dto = {
+        email: 'test@local.com',
+        password: 'wrong-password',
+      };
+      const res = await req.post('/api/v1/auth/login').send(dto);
+
+      expect(res.status).toEqual(400);
+    });
+
+    test('should return error with status code 400 for invalid email, username or password', async () => {
+      const dto = {
+        email: 'test',
+        password: 'wrong-password',
+      };
+      const res = await req.post('/api/v1/auth/login').send(dto);
+      expect(res.status).toEqual(400);
+    });
+  });
+
+  describe('Forgot password API', () => {
+    test('should send email with reset code when input email', async () => {
+      const dto = {
+        email: 'test@local.com',
+      };
+      const res = await req.post('/api/v1/auth/forgot-password').send(dto);
+
+      expect(res.status).toEqual(200);
+    });
+
+    test('should send email with reset code when input username', async () => {
+      const dto = {
+        username: 'test',
+      };
+      const res = await req.post('/api/v1/auth/forgot-password').send(dto);
+
+      expect(res.status).toEqual(200);
+    });
+
+    test('should return error with status code 404 for not found user', async () => {
+      const dto = {
+        username: 'test-not-exist',
+      };
+      const res = await req.post('/api/v1/auth/forgot-password').send(dto);
+
+      expect(res.status).toEqual(404);
+    });
+  });
+
+  describe('Reset password API', () => {
+    test('should reset password when input valid otp', async () => {
+      const dtoForgot = {
+        email: 'test@local.com',
+      };
+      const forgotRes = await req.post('/api/v1/auth/forgot-password').send(dtoForgot);
+      const { otp } = forgotRes.body.data;
+      const dtoReset = {
+        otp,
+        email: 'test@local.com',
+        newPassword: 'new-password',
+        rePassword: 'new-password',
+      };
+      const res = await req.post('/api/v1/auth/reset-password').send(dtoReset);
+
+      expect(res.status).toEqual(200);
+    });
+
+    test('should return error when input invalid otp', async () => {
+      const dtoForgot = {
+        username: 'test',
+      };
+      await req.post('/api/v1/auth/forgot-password').send(dtoForgot);
+      const dtoReset = {
+        otp: '111111',
+        username: 'test',
+        newPassword: 'new-password',
+        rePassword: 'new-password',
+      };
+      const res = await req.post('/api/v1/auth/reset-password').send(dtoReset);
+
+      expect(res.status).toEqual(400);
+      expect(res.body.message).toEqual('Invalid OTP code');
+    });
+
+    test('should return error with status code 404 for not found user', async () => {
+      const dto = {
+        email: 'test-not-exist@local.com',
+        newPassword: 'new-password',
+        rePassword: 'new-password',
+        otp: '122323',
+      };
+      const res = await req.post('/api/v1/auth/reset-password').send(dto);
+
+      expect(res.status).toEqual(404);
+    });
+  });
+});
--- src/configs/database/postgresql/index.ts
+++ src/configs/database/postgresql/index.ts
@@ -5,21 +5,25 @@ import logger from '@src/configs/logger';
 import { configDB } from './config.sequelize';
 
 config();
+const TEST_MODE = process.env.NODE_ENV === 'test';
 
 const DB_CONNECT_TIMEOUT = 10 * 1000;
 const DB_RECONNECT_TIMEOUT = 1 * 1000;
 let connectionTimeout: NodeJS.Timeout | null = null;
 let reconnectTimeout: NodeJS.Timeout | null = null;
 
-const sequelize = new Sequelize(configDB.database as string, configDB.username as string, configDB.password, {
-  ...configDB,
-  dialect: configDB.dialect as Dialect,
-});
+const sequelize = TEST_MODE
+  ? new Sequelize('sqlite::memory:', { logging: false })
+  : new Sequelize(configDB.database as string, configDB.username as string, configDB.password, {
+      ...configDB,
+      dialect: configDB.dialect as Dialect,
+    });
 
 export const connectDB = async () => {
   try {
+    // const alter = process.env.NODE_ENV !== 'development' || process.env.NODE_ENV === 'test';
     await sequelize.authenticate();
-    await sequelize.sync({ alter: process.env.NODE_ENV !== 'development' });
+    await sequelize.sync({ alter: true });
     if (reconnectTimeout) clearTimeout(reconnectTimeout);
     if (connectionTimeout) clearTimeout(connectionTimeout);
     logger.log('DB connected');
