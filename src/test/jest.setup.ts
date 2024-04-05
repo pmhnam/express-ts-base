b660c0e96cf480e19bf55ca9ba4a75276fa8e3ab
Otis Pham <otis@vinova.com.sg>
2024-04-05 09:20:36 +0700

    fix: add run seeds data before test

--- src/test/jest.setup.ts
+++ src/test/jest.setup.ts
@@ -1,7 +1,30 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable import/no-dynamic-require */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { afterAll, beforeAll } from '@jest/globals';
 import { db } from '@src/configs/database';
 import { syncDatabase } from '@src/configs/database/postgresql';
+import fs from 'fs';
+import path from 'path';
+import { Sequelize } from 'sequelize';
+
+async function runSeeders() {
+  const seedDir = path.join(__dirname, '../configs/database/seeders');
+  const seeds = fs.readdirSync(seedDir).filter((file) => file.endsWith('.js'));
+  const queryInterface = db.sequelize.getQueryInterface();
+
+  for (const seed of seeds) {
+    const seedFile = path.join(seedDir, seed);
+    const { up } = require(seedFile);
+    try {
+      await up(queryInterface, Sequelize);
+      // eslint-disable-next-line no-empty
+    } catch (error) {}
+  }
+}
 
 afterAll(async () => {
   await db.sequelize.close();
@@ -9,5 +32,6 @@ afterAll(async () => {
 });
 
 beforeAll(async () => {
-  await syncDatabase();
+  await syncDatabase({ force: true });
+  await runSeeders();
 });
